1bcac66ced8aaac22ca8eb3c50b5517678146355
namihira-k <kosuke.namihira@gmail.com>
2019-08-12 11:21:56 +0900

    to react for vue

---
+++ resources/views/react/body.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <p>React</p>
+  <div id="example"></div>
+
+
+<script src="{{ asset('/js/app.js') }}"></script>
+<script src="{{ asset('/js/react/script.js') }}"></script>
+</body>
+
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,8 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+ 
+mix.react('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.react('resources/js/components/Example.js', 'public/js/app.js');
