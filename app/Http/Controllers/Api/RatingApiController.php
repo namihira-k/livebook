145b8c2ee9e517a4105ab136e7dd74434542f72e
namihira-k <kosuke.namihira@gmail.com>
2020-02-11 16:35:21 +0900

    add rating msg log

--- app/Http/Controllers/Api/RatingApiController.php
+++ app/Http/Controllers/Api/RatingApiController.php
@@ -6,21 +6,35 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 use App\Http\Controllers\Controller;
+
+use App\Models\Comment;
 use App\Models\Rating;
+
 
 class RatingApiController extends Controller
 {
     public function post(Request $request)
     {
+      $comment_uuid = $request->comment_uuid;
       $rating = Rating::updateOrCreate(
         [
-          'comment_uuid' => $request->comment_uuid, 
+          'comment_uuid' => $comment_uuid, 
           'user_uuid' => $request->cookie('user_uuid'), 
           'label' => $request->label
         ]
       );
 
-      Log::info("POST : " . $rating->label);
+      $comment =  Comment::where('comments.uuid', $comment_uuid)
+                        ->leftJoin('events', 'comments.event_uuid', '=', 'events.uuid')
+                        ->select('comments.*', 'events.name as event_name')
+                        ->first();
+
+      Log::info("POST /ratings : " . 
+                  $rating->label . "\n" . 
+                  $comment->event_name . "\n" . 
+                  $comment->comment . "\n" .
+                  "https://liveshare.namimono.com/web/eventinfo?uuid=" . $comment->event_uuid 
+      );
       
       return response()->json($rating);
     }
